c8a0c741caa5fdbe54eb57fbb54a63dde54b1ba0
Mendel <mendel@Mendels-MacBook-Air.local>
2025-02-06 20:36:41 +0100

    new

--- map_reader.c
+++ map_reader.c
@@ -3,26 +3,88 @@
 /*                                                        :::      ::::::::   */
 /*   map_reader.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mteichma <mteichma@student.42.fr >         +#+  +:+       +#+        */
+/*   By: mteichma <mteichma@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/02/06 19:57:31 by mteichma          #+#    #+#             */
-/*   Updated: 2025/02/06 19:57:54 by mteichma         ###   ########.fr       */
+/*   Created: 2025/02/06 19:52:36 by mteichma          #+#    #+#             */
+/*   Updated: 2025/02/06 20:35:47 by mteichma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
+static int	validate_line_length(char *line, t_game *game)
+{
+	size_t	len;
+
+	if (!line || !game)
+		return (0);
+	len = 0;
+	while (line[len] && line[len] != '\n')
+		len++;
+	return (len == (size_t)game->width);
+}
+
 static int	process_line(char *line, t_game *game, int i)
 {
-	int	result;
-
-	result = check_and_store_line(line, game, i);
-	if (!result)
+	if (!line || !game || i < 0 || i >= game->height)
 	{
 		free(line);
-		free_map(game->map);
+		return (0);
+	}
+	if (!validate_line_length(line, game) || !check_and_store_line(line, game,
+			i))
+	{
+		free(line);
+		if (game->map)
+			free_map(game->map);
 		return (0);
 	}
 	free(line);
 	return (1);
+}
+
+static int	init_map_reading(char *filename, t_game *game, int *fd)
+{
+	if (!game || !filename)
+		return (0);
+	*fd = open_map_file(filename);
+	if (*fd == -1)
+		return (0);
+	if (!allocate_map(game))
+	{
+		close(*fd);
+		return (0);
+	}
+	return (1);
+}
+
+static int	process_map_lines(int fd, t_game *game)
+{
+	char	*line;
+	int		i;
+
+	i = 0;
+	line = get_next_line(fd);
+	while (line && i < game->height)
+	{
+		if (!process_line(line, game, i++))
+		{
+			close(fd);
+			return (0);
+		}
+		line = get_next_line(fd);
+	}
+	return (i);
+}
+
+int	read_map_file(char *filename, t_game *game)
+{
+	int	fd;
+	int	lines_read;
+
+	if (!init_map_reading(filename, game, &fd))
+		return (0);
+	lines_read = process_map_lines(fd, game);
+	close(fd);
+	return (lines_read == game->height && validate_map_content(game));
 }
--- path_check.c
+++ path_check.c
@@ -3,14 +3,29 @@
 /*                                                        :::      ::::::::   */
 /*   path_check.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mteichma <mteichma@student.42.fr >         +#+  +:+       +#+        */
+/*   By: mteichma <mteichma@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/06 19:48:09 by mteichma          #+#    #+#             */
-/*   Updated: 2025/02/06 19:48:31 by mteichma         ###   ########.fr       */
+/*   Updated: 2025/02/06 20:30:39 by mteichma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
+
+static void	free_map_copy(char **map_copy, int height)
+{
+	int	i;
+
+	if (!map_copy)
+		return ;
+	i = 0;
+	while (i < height)
+	{
+		free(map_copy[i]);
+		i++;
+	}
+	free(map_copy);
+}
 
 static char	**copy_map(t_game *game)
 {
@@ -28,9 +43,7 @@ static char	**copy_map(t_game *game)
 		copy[i] = ft_strdup(game->map[i]);
 		if (!copy[i])
 		{
-			while (i > 0)
-				free(copy[--i]);
-			free(copy);
+			free_map_copy(copy, i);
 			return (NULL);
 		}
 		i++;
@@ -86,12 +99,6 @@ int	is_path_valid(t_game *game)
 	result = check_reachable(map_copy, game);
 	if (!result)
 		ft_printf("Error\nNo valid path to all collectibles and exit\n");
-	while (map_copy[0])
-	{
-		free(*map_copy);
-		map_copy++;
-	}
-	map_copy -= game->height;
-	free(map_copy);
+	free_map_copy(map_copy, game->height);
 	return (result);
 }
