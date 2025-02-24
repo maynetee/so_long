a194f188fa683112d452c46af735a15ed883a162
Mendel <mendel@Mendels-MacBook-Air.local>
2025-02-24 18:36:15 +0100

    noooo

--- render_map.c
+++ render_map.c
@@ -3,54 +3,48 @@
 /*                                                        :::      ::::::::   */
 /*   render_map.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mteichma <mteichma@student.42.fr >         +#+  +:+       +#+        */
+/*   By: mteichma <mteichma@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/22 20:16:22 by mteichma          #+#    #+#             */
-/*   Updated: 2025/02/22 21:48:50 by mteichma         ###   ########.fr       */
+/*   Updated: 2025/02/24 18:35:44 by mteichma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-static void	render_floor(t_game *game, int x, int y)
+static void	render_tile(t_game *game, int x, int y)
 {
-	mlx_put_image_to_window(game->mlx, game->win, game->floor_img, x
-		* game->tile_size, y * game->tile_size);
-}
-
-static void	render_wall(t_game *game, int x, int y)
-{
-	mlx_put_image_to_window(game->mlx, game->win, game->wall_img, x
-		* game->tile_size, y * game->tile_size);
-}
-
-static void	render_exit(t_game *game, int x, int y)
-{
-	if (game->count_c == 0)
-		mlx_put_image_to_window(game->mlx, game->win, game->exit_open_img, x
-			* game->tile_size, y * game->tile_size);
+	if (game->map[y][x] == '1')
+		mlx_put_image_to_window(game->mlx, game->win, game->wall_img,
+			x * game->tile_size, y * game->tile_size);
 	else
-		mlx_put_image_to_window(game->mlx, game->win, game->exit_closed_img, x
-			* game->tile_size, y * game->tile_size);
+		mlx_put_image_to_window(game->mlx, game->win, game->floor_img,
+			x * game->tile_size, y * game->tile_size);
+	if (game->map[y][x] == 'C')
+	{
+		if ((game->global_frame / 30) % 2 == 0)
+			mlx_put_image_to_window(game->mlx, game->win, game->item_img_1,
+				x * game->tile_size, y * game->tile_size);
+		else
+			mlx_put_image_to_window(game->mlx, game->win, game->item_img_2,
+				x * game->tile_size, y * game->tile_size);
+	}
+	if (game->map[y][x] == 'E')
+	{
+		if (game->count_c == 0)
+			mlx_put_image_to_window(game->mlx, game->win, game->exit_open_img,
+				x * game->tile_size, y * game->tile_size);
+		else
+			mlx_put_image_to_window(game->mlx, game->win, game->exit_closed_img,
+				x * game->tile_size, y * game->tile_size);
+	}
 }
 
-static void	render_item(t_game *game, int x, int y)
+static void	render_entities(t_game *game)
 {
-	void	*item_img;
-
-	if ((game->global_frame / 30) % 2 == 0)
-		item_img = game->item_img_1;
-	else
-		item_img = game->item_img_2;
-	mlx_put_image_to_window(game->mlx, game->win, item_img, x * game->tile_size,
-		y * game->tile_size);
-}
-
-/* Retrait de static ici */
-void	render_enemies(t_game *game)
-{
-	void	*enemy_img;
 	int		i;
+	void	*enemy_img;
+	void	*player_img;
 
 	i = 0;
 	while (i < game->enemy_count)
@@ -60,16 +54,10 @@ void	render_enemies(t_game *game)
 		else
 			enemy_img = game->enemy_move_2;
 		mlx_put_image_to_window(game->mlx, game->win, enemy_img,
-			game->enemies[i].x * game->tile_size, game->enemies[i].y
-			* game->tile_size);
+			game->enemies[i].x * game->tile_size,
+			game->enemies[i].y * game->tile_size);
 		i++;
 	}
-}
-
-void	render_player(t_game *game)
-{
-	void	*player_img;
-
 	if (game->win_flag == 1)
 		player_img = game->player_win;
 	else if (game->win_flag == 2)
@@ -81,8 +69,9 @@ void	render_player(t_game *game)
 		else
 			player_img = game->player_idle_2;
 	}
-	mlx_put_image_to_window(game->mlx, game->win, player_img, game->player_x
-		* game->tile_size, game->player_y * game->tile_size);
+	mlx_put_image_to_window(game->mlx, game->win, player_img,
+		game->player_x * game->tile_size,
+		game->player_y * game->tile_size);
 }
 
 void	render_map(t_game *game)
@@ -96,19 +85,11 @@ void	render_map(t_game *game)
 		x = 0;
 		while (x < game->width)
 		{
-			if (game->map[y][x] == '1')
-				render_wall(game, x, y);
-			else
-				render_floor(game, x, y);
-			if (game->map[y][x] == 'C')
-				render_item(game, x, y);
-			if (game->map[y][x] == 'E')
-				render_exit(game, x, y);
+			render_tile(game, x, y);
 			x++;
 		}
 		y++;
 	}
-	render_enemies(game);
-	render_player(game);
+	render_entities(game);
 	render_move_count(game);
 }
--- window.c
+++ window.c
@@ -6,103 +6,45 @@
 /*   By: mteichma <mteichma@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/21 01:11:27 by mteichma          #+#    #+#             */
-/*   Updated: 2025/02/22 21:08:08 by mteichma         ###   ########.fr       */
+/*   Updated: 2025/02/24 18:35:59 by mteichma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-static void	load_images_part1(t_game *game)
+static void	draw_bg_row(char *addr, int row, int sl, int bpp)
 {
-	int	w;
-	int	h;
+	int	x;
 
-	game->wall_img = mlx_xpm_file_to_image(game->mlx, "assets/wall.xpm", &w,
-			&h);
-	game->floor_img = mlx_xpm_file_to_image(game->mlx, "assets/floor.xpm", &w,
-			&h);
-	game->exit_closed_img = mlx_xpm_file_to_image(game->mlx,
-			"assets/exit_closed.xpm", &w, &h);
-	game->exit_open_img = mlx_xpm_file_to_image(game->mlx,
-			"assets/exit_open.xpm", &w, &h);
-	game->item_img_1 = mlx_xpm_file_to_image(game->mlx, "assets/item_1.xpm", &w,
-			&h);
-}
-
-static void	load_images_part2(t_game *game)
-{
-	int	w;
-	int	h;
-
-	game->item_img_2 = mlx_xpm_file_to_image(game->mlx, "assets/item_2.xpm", &w,
-			&h);
-	game->player_idle_1 = mlx_xpm_file_to_image(game->mlx,
-			"assets/player_idle_1.xpm", &w, &h);
-	game->player_idle_2 = mlx_xpm_file_to_image(game->mlx,
-			"assets/player_idle_2.xpm", &w, &h);
-	game->player_move_1 = mlx_xpm_file_to_image(game->mlx,
-			"assets/player_move_1.xpm", &w, &h);
-	game->player_move_2 = mlx_xpm_file_to_image(game->mlx,
-			"assets/player_move_2.xpm", &w, &h);
-}
-
-void	load_images(t_game *game)
-{
-	int	w;
-	int	h;
-
-	load_images_part1(game);
-	load_images_part2(game);
-	game->player_win = mlx_xpm_file_to_image(game->mlx, "assets/player_win.xpm",
-			&w, &h);
-	game->player_dead = mlx_xpm_file_to_image(game->mlx,
-			"assets/player_dead.xpm", &w, &h);
-	game->enemy_move_1 = mlx_xpm_file_to_image(game->mlx,
-			"assets/enemy_move_1.xpm", &w, &h);
-	game->enemy_move_2 = mlx_xpm_file_to_image(game->mlx,
-			"assets/enemy_move_2.xpm", &w, &h);
-	if (!game->wall_img || !game->floor_img || !game->item_img_1
-		|| !game->item_img_2 || !game->player_idle_1 || !game->player_idle_2
-		|| !game->player_move_1 || !game->player_move_2 || !game->player_win
-		|| !game->exit_closed_img || !game->exit_open_img || !game->player_dead
-		|| !game->enemy_move_1 || !game->enemy_move_2)
+	x = 0;
+	while (x < 100)
 	{
-		ft_printf("Error\nFailed to load images\n");
-		close_game(game);
+		*(unsigned int *)(addr + (row * sl + x * (bpp / 8))) = 0x000000;
+		x++;
 	}
 }
 
-void	setup_window(t_game *game)
+void	create_move_count_bg_image(t_game *game)
 {
-	game->mlx = mlx_init();
-	if (!game->mlx)
+	int		row;
+	int		bpp;
+	int		sl;
+	int		endian;
+	char	*addr;
+
+	game->move_bg_img = mlx_new_image(game->mlx, 100, 40);
+	if (!game->move_bg_img)
+		return ;
+	addr = mlx_get_data_addr(game->move_bg_img, &bpp, &sl, &endian);
+	row = 0;
+	while (row < 40)
 	{
-		ft_printf("Error\nFailed to init MiniLibX\n");
-		close_game(game);
+		draw_bg_row(addr, row, sl, bpp);
+		row++;
 	}
-	game->win = mlx_new_window(game->mlx, 1280, 704, "So_Long");
-	if (!game->win)
-	{
-		ft_printf("Error\nFailed to create window\n");
-		close_game(game);
-	}
-	game->scale_x = 1280 / game->width;
-	game->scale_y = 720 / game->height;
-	if (game->scale_x < game->scale_y)
-		game->tile_size = game->scale_x;
-	else
-		game->tile_size = game->scale_y;
 }
 
-void	init_window(t_game *game)
+void	display_message(t_game *game, char *message)
 {
-	setup_window(game);
-	load_images(game);
-	create_move_count_bg_image(game);
-	render_map(game);
-	mlx_hook(game->win, 17, 0, close_game_wrapper, game);
-	mlx_hook(game->win, 2, 1L << 0, handle_keypress, game);
-	mlx_do_key_autorepeaton(game->mlx);
-	mlx_loop_hook(game->mlx, update_game, game);
-	mlx_loop(game->mlx);
+	mlx_string_put(game->mlx, game->win, 10, 60, 0xFFFFFF, message);
 }
